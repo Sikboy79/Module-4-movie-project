79ba4a69b93d87c3b0ee5fa3a9a05ed633ac6ce8
Sikboy79 <custombasewelds@yahoo.com>
2025-09-09 16:24:39 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,78 @@
+
+const moviesWrapper = document.querySelector(".movies");
+const searchName = document.querySelector(".searchName");
+
+let currentMovies = [];
+
+function searchChange(event) {
+  renderMovies(event.target.value);
+  searchName.innerHTML = event.target.value;
+}
+
+function showSkeletons(count = 6) {
+    moviesWrapper.innerHTML = "";
+    for (let i = 0; i < count; i++) {
+        const skeleton = document.createElement("div");
+        skeleton.classList.add("movie", "movies__loading--skeleton");  //SKELETON PLACEHOLDER DISPLAY
+        moviesWrapper.appendChild(skeleton);  
+    }
+}
+
+//RENDERING MOVIES
+async function renderMovies(searchTerm) {
+   showSkeletons(6)  //SKELETON LOADING
+
+  const response = await fetch(
+    `http://www.omdbapi.com/?s=${searchTerm}&apikey=6cc6546`  //API PULL
+  );
+
+  moviesWrapper.classList.remove('movies__loading--skeleton');  //SKELETON LOADING
+
+  const data = await response.json();  //CONVERT TO HTML
+  currentMovies = data.Search;
+  displayMovies(currentMovies);
+}
+
+//DISPLAYING MOVIES
+
+function displayMovies(movieList) {
+  moviesWrapper.innerHTML = movieList
+    .slice(0, 6)
+    .map((movie) => {
+      return `
+        <div class="movie">
+        <img src=${movie.Poster}alt="" />
+        <h2>${movie.Title}</h2>
+        <h4>${movie.Year}</h4>
+        <button>Learn More</button>
+        </div>
+        `;
+    })
+    .join("");
+}
+
+// DISPLAYING MOVIES
+
+function sortChange(event) {
+  const sortOption = event.target.value;
+
+  let sortedMovies = [...currentMovies];
+
+  if (sortOption === "newest") {
+    sortedMovies.sort((a, b) => b.Year - a.Year);
+  } else if (sortOption === "oldest") {
+    sortedMovies.sort((a, b) => a.Year - b.Year);
+  }
+  displayMovies(sortedMovies);
+}
+
+//SKELETON
+
+function getMovies() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve([
+                {currentMovies},
+            ]);
+        }), 3000}
+    )};
